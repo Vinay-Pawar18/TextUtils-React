a73863065875e6ba05ce74fd086909918d687dd7
Vinay Pawar <pawarvinay1996@gmail.com>
2022-03-25 12:00:26 +0530

    setup web app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] =useState("light"); // Whether the dark mode is enabled or not
@@ -41,21 +41,21 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
      {/* <Navbar title="Textutils" about="AboutUtils"/> */}
      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
      <Alert alert={alert}/>
      <div className='container my-3'>
-        <Switch>
+        {/* <Switch>
               <Route path="/about">
                 <About />
               </Route>
-              <Route path="/">
+              <Route path="/"> */}
               <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-              </Route>
-        </Switch>
+              {/* </Route>
+        </Switch> */}
      </div>
-     </Router>
+     {/* </Router> */}
     </>
   );
 }
